9fdb3d625d5c6457db8545c349ffbddd6a7d9eb6
ilhammursidi <imursidi37@gmail.com>
2026-04-06 22:23:01 +0700

    docs: add jsdocs for Modal function

--- src/Components/Atoms/Modal.jsx
+++ src/Components/Atoms/Modal.jsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+/**
+ * 
+ * @typedef {Object} ModalProps
+ * @param {Object} props - The properties for the Modal component
+ * @param {boolean} props.isOpen - A boolean indicating whether the modal is open or not 
+ * @param {string} props.value - The text for the modal's close button
+ * @param {Function} props.onClose - The function to call when the modal is closed
+ * @param {React.ReactNode} props.children - The content to display inside the modal
+ * @returns {JSX.Element} The Modal component
+ */
 
-export const Modal = ({isOpen, onClose, children}) => {
+export const Modal = ({isOpen,value, onClose, children}) => {
     if(!isOpen) return null;
     return (
         <section>
-            <section className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+            <section className="fixed top-10 inset-0 bg-black/60 flex items-center justify-center">
                 <section className="bg-white p-5 rounded-lg">
                     {children}
-                    <button onClick={onClose} className="mt-5 bg-blue-600 text-white px-4 py-2 rounded-md">Close</button>
+                    <button onClick={onClose} className="mt-5 bg-blue-600 text-white w-full px-4 py-2 rounded-md">{value}</button>
                 </section>
             </section>
         </section>
