19b0b444cefba8743ac1414a6a9b06a6a43dc658
ilhammursidi <imursidi37@gmail.com>
2026-04-08 16:21:35 +0700

    feat: add toaster

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import AppRouter from './AppRouter.jsx'
 import "/src/Style/Global.css"
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <AppRouter />
+    <Toaster/>
     </BrowserRouter>
   </StrictMode>,
 )
