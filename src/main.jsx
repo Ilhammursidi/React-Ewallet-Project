c75b055db77196a10a65071015605d9486cede4f
ilhammursidi <imursidi37@gmail.com>
2026-04-15 06:58:14 +0700

    feat: add loading for persistor

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,19 @@ import { createRoot } from 'react-dom/client'
 import AppRouter from './AppRouter.jsx'
 import "/src/Style/Global.css"
 import { Toaster } from 'react-hot-toast'
+import store, { persistor } from './Redux/store.js'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+      <PersistGate loading={<h1>Loading...</h1>} persistor={persistor}>
   <StrictMode>
     <BrowserRouter>
     <AppRouter />
     <Toaster/>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
+      </PersistGate>
+    </Provider>,
 )
