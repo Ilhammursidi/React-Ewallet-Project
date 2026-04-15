596bb6ecb7a4682640085bcfb6ea1c3ea45fce0b
ilhammursidi <imursidi37@gmail.com>
2026-04-15 07:04:43 +0700

    feat: add selector and dispatch for signup

--- src/Pages/Auth/SignUp.jsx
+++ src/Pages/Auth/SignUp.jsx
@@ -1,105 +1,106 @@
-import { NavLink } from "react-router";
-import { Button } from "../../Components/Atoms/Button";
-import { SocialButton } from "../../Components/Atoms/SocialButton";
-import { Logo } from "../../Components/Atoms/Logo";
-import { InputEmail } from "../../Components/Form/InputEmail";
-import { InputPassword } from "../../Components/Form/InputPassword";
-import { ConfirmPassword } from "../../Components/Form/ConfirmPassword";
-import { useState } from "react";
-import { useNavigate } from "react-router";
-import toast from "react-hot-toast";
-import { useDispatch, useSelector } from "react-redux";
-import { addUser } from "../../Redux/slice/register";
+    import { NavLink } from "react-router";
+    import { Button } from "../../Components/Atoms/Button";
+    import { SocialButton } from "../../Components/Atoms/SocialButton";
+    import { Logo } from "../../Components/Atoms/Logo";
+    import { InputEmail } from "../../Components/Form/InputEmail";
+    import { InputPassword } from "../../Components/Form/InputPassword";
+    import { ConfirmPassword } from "../../Components/Form/ConfirmPassword";
+    import { useState } from "react";
+    import { useNavigate } from "react-router";
+    import toast from "react-hot-toast";
+    import { useDispatch, useSelector } from "react-redux";
+    import { addUser } from "../../Redux/slice/register";
 
-function SignUp() {
-    const { allUsers } = useSelector((state) => state.registration) || [];
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [confirm, setConfirm] = useState("")
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    
+    function SignUp() {
+        const allUsers = useSelector((state) => state.addUser.allUsers) || [];
+        const [email, setEmail] = useState("")
+        const [password, setPassword] = useState("")
+        const [confirm, setConfirm] = useState("")
+        const navigate = useNavigate();
+        const dispatch = useDispatch();
+        
 
-    const handleRegister = (e) => {
-    e.preventDefault();
+        const handleRegister = (e) => {
+        e.preventDefault();
 
-    const isEmailExist = allUsers?.some(
-        (acc) => acc.email === email.trim().toLowerCase()
-    );
+        const isEmailExist = allUsers?.some(
+            (acc) => acc.email === email.trim().toLowerCase()
+        );
 
-    if (!email || !password || !confirm) {
-        toast.error("All fields are required");
-        return;
+        if (!email || !password || !confirm) {
+            toast.error("All fields are required");
+            return;
+        }
+
+        if (isEmailExist) {
+            toast.error("email already exist");
+            return;
+        }
+
+        dispatch(addUser({
+            email: email.trim().toLowerCase(),
+            password,
+            userPin: null
+        }));
+
+        setEmail("");
+        setPassword("");
+        setConfirm("");
+
+        toast.success("Account Saved Successfully");
+        setTimeout(() => {
+            navigate("/auth/login");
+        },1500)
+    };
+
+
+        return (
+            <main>
+                <section className="flex min-h-screen overflow-hidden bg-blue-600">
+
+                    <section className="left-side bg-white md:rounded-r-4xl px-6 py-10 md:w-1/2 md:px-10">
+                        <Logo className="text-xl" color="blue" />
+                        <p className="text-xl font-medium py-3">Start Accessing Banking Needs
+                            With All Devices and All Platforms
+                            With 30.000+ Users</p>
+                        <p className="text-gray-500 text-sm">Transfering money is eassier than ever, you can access Zwallet wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!</p>
+                        <section className="flex flex-row gap-5 py-5  md:flex-col">
+                            <SocialButton className="w-full p-2 md:flex md:flex-row md:gap-2">
+                                <img className="w-7 md:w-5 md:m-0 m-auto md:ml-auto" src="/icons/googleround.svg" alt="google-icon" />
+                                <p className="hidden md:flex md:mr-auto">Sign Up With Google</p>
+                            </SocialButton>
+                            <SocialButton className="w-full p-2 md:flex md:flex-row md:gap-2 ">
+                                <img className="w-9 m-auto md:ml-auto md:w-6 md:m-0" src="/icons/fbround.svg" alt="facebook-icon" />
+                                <p className="hidden md:flex md:mr-auto">Sign Up With Facebook</p>
+                            </SocialButton>
+                        </section>
+                        <section className="flex flex-row items-center justify-between">
+                            <hr className="border border-gray-300 w-[40%]" />
+                            <p className="text-gray-400" >Or</p>
+                            <hr className="border border-gray-300 w-[40%]" />
+                        </section>
+                        <form noValidate onSubmit={handleRegister}>
+                            <InputEmail 
+                            value={email} onChange={e => setEmail(e.target.value)}></InputEmail>
+                            <InputPassword value={password} onChange={e => setPassword(e.target.value)}></InputPassword>
+                            <ConfirmPassword value={confirm} onChange={e => setConfirm(e.target.value)} password={password}></ConfirmPassword>
+                            <Button type="submit" className="w-full h-12 py-2 mt-4" color="blue">
+                                Register
+                            </Button>
+                            <p className="text-center mt-2 text-gray-600">Have An Account?
+                                <NavLink
+                                    className="text-blue-600" to={"/auth/login"}> Login
+                                </NavLink>
+                            </p>
+                        </form>
+                    </section>
+                    <section className="hidden items center right-side bg-blue-600 md:flex md:w-1/2 justify-center">
+                        <img src="/icons/handwallet.svg" alt="hand and wallet" />
+                    </section>
+                </section>
+            </main>
+        )
     }
 
-    if (isEmailExist) {
-        toast.error("email already exist");
-        return;
-    }
-
-    dispatch(addUser({
-        email: email.trim().toLowerCase(),
-        password,
-        userPin: null
-    }));
-
-    setEmail("");
-    setPassword("");
-    setConfirm("");
-
-    toast.success("Account Saved Successfully");
-
-    navigate("/auth/login");
-};
-
-
-    return (
-        <main>
-            <section className="flex min-h-screen overflow-hidden bg-blue-600">
-
-                <section className="left-side bg-white md:rounded-r-4xl px-6 py-10 md:w-1/2 md:px-10">
-                    <Logo className="text-xl" color="blue" />
-                    <p className="text-xl font-medium py-3">Start Accessing Banking Needs
-                        With All Devices and All Platforms
-                        With 30.000+ Users</p>
-                    <p className="text-gray-500 text-sm">Transfering money is eassier than ever, you can access Zwallet wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!</p>
-                    <section className="flex flex-row gap-5 py-5  md:flex-col">
-                        <SocialButton className="w-full p-2 md:flex md:flex-row md:gap-2">
-                            <img className="w-7 md:w-5 md:m-0 m-auto md:ml-auto" src="/icons/googleround.svg" alt="google-icon" />
-                            <p className="hidden md:flex md:mr-auto">Sign Up With Google</p>
-                        </SocialButton>
-                        <SocialButton className="w-full p-2 md:flex md:flex-row md:gap-2 ">
-                            <img className="w-9 m-auto md:ml-auto md:w-6 md:m-0" src="/icons/fbround.svg" alt="facebook-icon" />
-                            <p className="hidden md:flex md:mr-auto">Sign Up With Facebook</p>
-                        </SocialButton>
-                    </section>
-                    <section className="flex flex-row items-center justify-between">
-                        <hr className="border border-gray-300 w-[40%]" />
-                        <p className="text-gray-400" >Or</p>
-                        <hr className="border border-gray-300 w-[40%]" />
-                    </section>
-                    <form noValidate onSubmit={handleRegister}>
-                        <InputEmail 
-                        value={email} onChange={e => setEmail(e.target.value)}></InputEmail>
-                        <InputPassword value={password} onChange={e => setPassword(e.target.value)}></InputPassword>
-                        <ConfirmPassword value={confirm} onChange={e => setConfirm(e.target.value)} password={password}></ConfirmPassword>
-                        <Button type="submit" className="w-full h-12 py-2 mt-4" color="blue">
-                            Register
-                        </Button>
-                        <p className="text-center mt-2 text-gray-600">Have An Account?
-                            <NavLink
-                                className="text-blue-600" to={"/auth/login"}> Login
-                            </NavLink>
-                        </p>
-                    </form>
-                </section>
-                <section className="hidden items center right-side bg-blue-600 md:flex md:w-1/2 justify-center">
-                    <img src="/icons/handwallet.svg" alt="hand and wallet" />
-                </section>
-            </section>
-        </main>
-    )
-}
-
-export default SignUp
-;
+    export default SignUp
+    ;
