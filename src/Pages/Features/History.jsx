1805559624013ffbbc0870289dc8bcd2dcf20351
ilhammursidi <imursidi37@gmail.com>
2026-04-19 21:02:13 +0700

    feat: add new data and pagination

--- src/Pages/Features/History.jsx
+++ src/Pages/Features/History.jsx
@@ -18,61 +18,163 @@ export const History = () => {
     const [selectData, setSelectData] = useState(null);
     
     const query = searchParams.get("search") || "";
+    const currentPage = parseInt(searchParams.get("page") || "1")
+    const itemsPerPage = 5;
     const data = [
         {
             id: 1,
             name: "Ghaluh 1",
-            phone: "(239) 555-2208",
+            phone: "082134348877",
             img: "/icons/Table cell.svg",
-            amount: "Rp.50.000",
+            amount: "Rp.50.000"
         },
         {
             id: 2,
             name: "Jhon Cena",
-            phone: "(239) 555-0178",
+            phone: "(239) 555-0108",
             img: "/icons/Table cell-1.svg",
-            amount: "Rp.50.000",
+            amount: "Rp.50.000"
         },
         {
             id: 3,
             name: "Young Lex",
-            phone: "(239) 552-5532",
+            phone: "(239) 453-7432",
             img: "/icons/Table cell-2.svg",
-            amount: "Rp.50.000",
+            amount: "Rp.50.000"
         },
         {
             id: 4,
             name: "Bruno Mars",
-            phone: "(239) 555-0222",
+            phone: "(239) 009-9231",
             img: "/icons/Table cell-3.svg",
-            amount: "Rp.50.000",
+            amount: "Rp.50.000"
         },
         {
             id: 5,
             name: "Bruno Pluto",
-            phone: "(239) 555-0108",
+            phone: "(239) 555-2000",
             img: "/icons/Table cell-4.svg",
-            amount: "Rp.50.000",
+            amount: "Rp.50.000"
         },
         {
             id: 6,
             name: "Bruno Bogor",
-            phone: "089233812738",
+            phone: "081524249988",
             img: "/icons/Table cell-5.svg",
-            amount: "Rp.50.000",
+            amount: "Rp.50.000"
         },
         {
             id: 7,
             name: "Bruno G Putri",
-            phone: "082189961172",
+            phone: "(239) 555-0108",
             img: "/icons/Table cell-6.svg",
-            amount: "Rp.50.000",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 8,
+            name: "Bruno wanher",
+            phone: "(239) 555-2000",
+            img: "/icons/Table cell-4.svg",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 9,
+            name: "Bruno D Garut",
+            phone: "081524249988",
+            img: "/icons/Table cell-5.svg",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 10,
+            name: "Bruno G Mau",
+            phone: "(239) 555-0108",
+            img: "/icons/Table cell-6.svg",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 11,
+            name: "Bruno Dulu",
+            phone: "(239) 555-2000",
+            img: "/icons/Table cell-4.svg",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 12,
+            name: "Bruno Asin",
+            phone: "081524249988",
+            img: "/icons/Table cell-5.svg",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 13,
+            name: "Bruno D Luar",
+            phone: "(239) 555-0108",
+            img: "/icons/Table cell-6.svg",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 14,
+            name: "Luffy D depan",
+            phone: "(239) 555-2000",
+            img: "/icons/Table cell-4.svg",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 15,
+            name: "Bruno Tegal",
+            phone: "081524249988",
+            img: "/icons/Table cell-5.svg",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 16,
+            name: "Bruno Jombang",
+            phone: "(239) 555-0108",
+            img: "/icons/Table cell-6.svg",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 17,
+            name: "Ali D Luar",
+            phone: "(239) 555-0108",
+            img: "/icons/Table cell-6.svg",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 18,
+            name: "Pando D depan",
+            phone: "(239) 555-2000",
+            img: "/icons/Table cell-4.svg",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 19,
+            name: "Vina Tegal",
+            phone: "081524249988",
+            img: "/icons/Table cell-5.svg",
+            amount: "Rp.50.000"
+        },
+        {
+            id: 20,
+            name: "Siska Jombang",
+            phone: "(239) 555-0108",
+            img: "/icons/Table cell-6.svg",
+            amount: "Rp.50.000"
         }
     ];
-
     const filteredData = data.filter((row) => {
         return row.name.toLowerCase().includes(query.toLowerCase()) || row.phone.includes(query);
     });
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem)
+
+    const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+
+    const paginate = (pageNumber) => {
+        setSearchParams({ search: query, page: pageNumber.toString() });
+    };
 
     const handleRowClick = (row) => {
         setIsOpen(true);
@@ -108,9 +210,7 @@ export const History = () => {
                 <section className="md:flex md:justify-between w-full">
             <SideBar></SideBar>
             <section className="hidden md:block w-50"></section>
-            
-
-                
+             
                 <section className="md:flex flex-col md:w-5/6 md:px-5">
                     <section className="hidden md:flex md:h-5 md:mr-auto md:gap-2 items-center font-medium md:m-5 ">
                         <img src="/icons/blueHistory.svg" alt="history" />
@@ -139,7 +239,7 @@ export const History = () => {
                     <table className="w-full [&_tr:nth-child(2n+1)]:bg-gray-100">
                         <tbody>
 
-                            {filteredData.map((row) => (
+                            {currentItems.map((row) => (
                                 <tr onClick={() => handleRowClick(row)} key={row.id} className="table-layout w-full text-xs">
                                     <td><img src={row.img} alt={row.name} /></td>
                                     <td>{row.name}</td>
@@ -153,6 +253,27 @@ export const History = () => {
 
                         </tbody>
                     </table>
+                    <section className="flex justify-center items-center gap-4 mt-6">
+                        <button
+                            disabled={currentPage === 1}
+                            onClick={() => paginate(currentPage - 1)}
+                            className="px-3 py-1 border rounded disabled:opacity-30"
+                        >
+                            Prev
+                        </button>
+
+                        <span className="text-sm">
+                            Page <strong>{currentPage}</strong> of {totalPages}
+                        </span>
+
+                        <button
+                            disabled={currentPage === totalPages || totalPages === 0}
+                            onClick={() => paginate(currentPage + 1)}
+                            className="px-3 py-1 border rounded disabled:opacity-30"
+                        >
+                            Next
+                        </button>
+                    </section>
                 </section>
                     </section>
 
