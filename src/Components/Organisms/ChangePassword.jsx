a17a94c9d2377c22ba1dd4d4ad70439abafc9c59
ilhammursidi <imursidi37@gmail.com>
2026-04-21 02:00:55 +0700

    feat: add logi for change Password

--- src/Components/Organisms/ChangePassword.jsx
+++ src/Components/Organisms/ChangePassword.jsx
@@ -2,8 +2,55 @@ import { SideBar } from "../Atoms/SideBar";
 import { AppHeader } from "./AppHeader";
 import { InputChange } from "../Form/ChangePwd";
 import { Button } from "../Atoms/Button";
+import { updatePassword } from "../../Redux/slice/register";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateCurrentUser } from "../../Redux/slice/authslice";
+import { useNavigate } from "react-router";
+import toast from "react-hot-toast";
 
 export function ChangePassword() {
+    const userLogin = useSelector((state) => state.auth.currentUser)
+    const [oldPassword, setOldPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+const handleResetPassword = () => {
+    if (!oldPassword || !newPassword || !confirmPassword) {
+        toast.error("Isi semua field!");
+        return;
+    }
+
+    if (oldPassword !== userLogin.password) {
+        toast.error("Password lama salah!");
+        return;
+    }
+
+    if (newPassword !== confirmPassword) {
+        toast.error("Password tidak sama!");
+        return;
+    }
+
+    dispatch(updatePassword({
+        email: userLogin.email,
+        newPassword
+    }));
+
+    dispatch(updateCurrentUser({
+        password: newPassword
+    }));
+
+    toast.success("Password berhasil diubah!");
+    navigate("/profile")
+
+    setOldPassword("");
+    setNewPassword("");
+    setConfirmPassword("");
+};
+
+
     return (
         <section>
             <AppHeader className="md:bg-white"></AppHeader>
@@ -12,10 +59,20 @@ export function ChangePassword() {
                     <p className="hidden md:block md:ml-70 md:mb-2 font-medium">Profile</p>
                     <section className="md:w-4/6 md:ml-70 md:border md:p-5">
                         <p className="font-medium">Change Password</p>
-                        <InputChange label="Existing Password"></InputChange>    
-                        <InputChange label="New Password"></InputChange>    
-                        <InputChange label="Confirm New Password"></InputChange>
-                        <Button className="mt-2 w-full" color="blue">Submit</Button>    
+                        <form onSubmit={handleResetPassword}>
+                        <InputChange
+                        value={oldPassword}
+                        onChange={(e)=>setOldPassword(e.target.value)} 
+                        label="Existing Password"></InputChange>    
+                        <InputChange label="New Password"
+                        value={newPassword}
+                        onChange={(e)=>setNewPassword(e.target.value)}></InputChange>    
+                        <InputChange label="Confirm New Password"
+                        value={confirmPassword}
+                        onChange={(e)=>setConfirmPassword(e.target.value)}></InputChange>
+                        <Button className="mt-2 w-full" color="blue" type="submit"
+                        >Submit</Button>    
+                        </form>
                     </section>
                 </section>
         </section>
