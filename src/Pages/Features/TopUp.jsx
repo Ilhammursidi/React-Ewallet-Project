376748899bc7d910a7e824c9a05c57a77d21d15b
ilhammursidi <imursidi37@gmail.com>
2026-04-25 00:31:38 +0700

    feat: add modal for confirm payment

--- src/Pages/Features/TopUp.jsx
+++ src/Pages/Features/TopUp.jsx
@@ -8,39 +8,41 @@ import { topUp } from "../../Redux/slice/register"
 import { updateBalance } from "../../Redux/slice/authslice"
 import toast from "react-hot-toast"
 import { updateCurrentUser } from "../../Redux/slice/authslice"
+import { Modal } from "../../Components/Atoms/Modal"
 
 
 export const TopUp = () => {
     const user = useSelector((state) => state.auth.currentUser)
     const accounts = useSelector((state) => state.users.users)
     const [order, setOrder] = useState(0);
+    const [open, setOpen] = useState(false);
     const dispatch = useDispatch();
 
     const payment = [
         {
-            for:"bri",
-            method:"Bank Rakyat Indonesia",
-            img:"/icons/Bank BRI (Bank Rakyat Indonesia) Logo (SVG-240p) - FileVector69 1.svg"
-        },
-        {   
-            for:"dana",
-            method:"Dana",
-            img:"/icons/Logo DANA (PNG-240p) - FileVector69 1.svg"
+            for: "bri",
+            method: "Bank Rakyat Indonesia",
+            img: "/icons/Bank BRI (Bank Rakyat Indonesia) Logo (SVG-240p) - FileVector69 1.svg"
         },
         {
-            for:"bca",
-            method:"Bank Central Asia",
-            img:"/icons/Bank BCA Logo (SVG-240p) - FileVector69 1.svg"
+            for: "dana",
+            method: "Dana",
+            img: "/icons/Logo DANA (PNG-240p) - FileVector69 1.svg"
         },
         {
-            for:"gopay",
-            method:"Gopay",
-            img:"/icons/Logo GoPay (SVG-240p) - FileVector69 1.svg"
+            for: "bca",
+            method: "Bank Central Asia",
+            img: "/icons/Bank BCA Logo (SVG-240p) - FileVector69 1.svg"
         },
         {
-            for:"ovo",
-            method:"Ovo",
-            img:"/icons/OVO.svg"
+            for: "gopay",
+            method: "Gopay",
+            img: "/icons/Logo GoPay (SVG-240p) - FileVector69 1.svg"
+        },
+        {
+            for: "ovo",
+            method: "Ovo",
+            img: "/icons/OVO.svg"
         }
     ]
 
@@ -83,6 +85,7 @@ export const TopUp = () => {
         }));
         setOrder("");
         toast.success("Top Up berhasil!");
+        setOpen(false)
     }
 
     return (
@@ -119,37 +122,45 @@ export const TopUp = () => {
                                 <p className="text-xs text-gray-600">Choose your payment method for top up account</p>
                                 {payment.map((p) =>
                                     <article className="flex text-sm items-center rounded-xl focus-within:border-blue-600 focus-within:border-2 cursor-pointer bg-gray-100 h-15 p-2 px-5 flex-row gap-2">
-                                    <input type="radio" id={p.for} name="payment" />
-                                    <img className="w-8" src={p.img} alt={p.method} />
-                                    <label htmlFor={p.for} className="w-full">{p.method}</label>
-                                </article>
+                                        <input type="radio" id={p.for} name="payment" />
+                                        <img className="w-8" src={p.img} alt={p.method} />
+                                        <label htmlFor={p.for} className="w-full">{p.method}</label>
+                                    </article>
                                 )}
-                                
+
                             </form>
                         </section>
                     </section>
                 </section>
-
+                <Modal isOpen={open} className="top-14" onClose={() => setOpen(false)}>
+                    <section className="w-80 h-40 m-auto rounded-md p-6 bg-white">
+                        <p className="text-center font-medium">Konfirmasi top-up: Rp {rupiah} Apakah nominalnya sudah sesuai?  ?</p>
+                        <section className="justify-between gap-2 flex">
+                            <Button color="blue" onClick={handleSubmit} className="font-semibold w-full text-white">Ya</Button>
+                            <Button color="white" onClick={() => setOpen(false)} className="font-semibold w-full  text-blue-600">Tidak</Button>
+                        </section>
+                    </section>
+                </Modal>
                 <section className="font-semibold p-5 md:mr-5 md:mt-16 md:border md:border-gray-400 md:h-fit flex flex-col gap-2">
                     <p className="font-medium pt-2">Payment</p>
                     <article className="flex p-2 justify-between">
                         <p>order</p>
-                        <p>Rp.{rupiah}</p>
+                        <p>Rp {rupiah}</p>
                     </article>
                     <article className="flex p-2 justify-between">
                         <p>Delivery</p>
-                        <p>Rp.0</p>
+                        <p>Rp 0</p>
                     </article>
                     <article className="flex p-2 justify-between">
                         <p>Tax</p>
-                        <p>Rp.0</p>
+                        <p>Rp 0</p>
                     </article>
                     <hr className="text-gray-400 m-2" />
                     <article className="flex p-2 justify-between">
                         <p>Sub Total</p>
-                        <p>Rp.{rupiah}</p>
+                        <p>Rp {rupiah}</p>
                     </article>
-                    <Button onClick={handleSubmit} color="blue" className="font-medium m-2">Submit</Button>
+                    <Button onClick={() => setOpen(true)} color="blue" className="font-medium m-2">Submit</Button>
                     <p className="font-normal text-sm p-2 text-gray-500">*Get Discount if you pay with Bank Central Asia</p>
                 </section>
             </section>
