e036d037df681c2372d9e64b71f1825d03aca1ce
ilhammursidi <imursidi37@gmail.com>
2026-04-26 13:32:58 +0700

    style: make sidebar justify-start

--- src/Components/Atoms/SideBar.jsx
+++ src/Components/Atoms/SideBar.jsx
@@ -12,35 +12,35 @@ export const SideBar = ({className}) => {
 
     return (
         <aside className={`${defaultClass} ${className}`}>
-            <NavLink to="/dashboard" className={({ isActive }) => isActive ? "bg-blue-600 flex justify-center gap-2 text-white px-5 py-2 rounded" : "text-gray-600 flex justify-center gap-2 hover:bg-gray-200 px-3 py-2 rounded"}>
+            <NavLink to="/dashboard" className={({ isActive }) => isActive ? "bg-blue-600 flex justify-start gap-2 text-white px-3 py-2 rounded" : "text-gray-600 flex justify-start gap-2 hover:bg-gray-200 px-3 py-2 rounded"}>
                 {({ isActive }) =>
                 <>
                 <img className={`w-5 h-5 transition ${ isActive ? "invert brightness-0" : "" }`} src="/icons/dashboard-two.svg" alt="dashboard" /><span>Dasboard</span>
                 </>
                 }
             </NavLink>
-            <NavLink to="/transfer" className={({ isActive }) => isActive ? "bg-blue-600 flex justify-center gap-2 text-white px-5 py-2 rounded" : "text-gray-600 flex justify-center gap-2 hover:bg-gray-200 px-3 py-2 rounded"}>
+            <NavLink to="/transfer" className={({ isActive }) => isActive ? "bg-blue-600 flex justify-start gap-2 text-white px-3 py-2 rounded" : "text-gray-600 flex justify-start gap-2 hover:bg-gray-200 px-3 py-2 rounded"}>
                 {({ isActive }) =>
                 <>
                 <img className={`w-5 h-5 transition ${ isActive ? "invert brightness-0" : "" }`} src="/icons/Send.svg" alt="transfer" /><span>Transfer</span>
                 </>
                 }
             </NavLink>
-            <NavLink to="/history" className={({ isActive }) => isActive ? "bg-blue-600 flex justify-center gap-2 text-white px-5 py-2 rounded" : "text-gray-600 flex justify-center gap-2 hover:bg-gray-200 px-3 py-2 rounded"}>
+            <NavLink to="/history" className={({ isActive }) => isActive ? "bg-blue-600 flex justify-start gap-2 text-white px-3 py-2 rounded" : "text-gray-600 flex justify-start gap-2 hover:bg-gray-200 px-3 py-2 rounded"}>
                {({ isActive }) =>
                 <>
                 <img className={`w-5 h-5 transition ${ isActive ? "invert brightness-0" : "" }`} src="/icons/history.svg" alt="history" /><span>History</span>
                 </>
                 }
             </NavLink>
-            <NavLink to="/topup" className={({ isActive }) => isActive ? "bg-blue-600 flex justify-center gap-2 text-white px-5 py-2 rounded" : "text-gray-600 flex justify-center gap-2 hover:bg-gray-200 px-3 py-2 rounded"}>
+            <NavLink to="/topup" className={({ isActive }) => isActive ? "bg-blue-600 flex justify-start gap-2 text-white px-3 py-2 rounded" : "text-gray-600 flex justify-start gap-2 hover:bg-gray-200 px-3 py-2 rounded"}>
             {({ isActive }) =>
                 <>
                 <img className={`w-5 h-5 transition ${ isActive ? "invert brightness-0" : "" }`} src="/icons/Upload.svg" alt="topup" /><span>Top Up</span>
                 </>
                 }
             </NavLink>
-            <NavLink to="/profile" className={({ isActive }) => isActive ? "bg-blue-600 flex justify-center gap-2 text-white px-5 py-2 rounded" : "text-gray-600 flex justify-center gap-2 hover:bg-gray-200 px-3 py-2 rounded"}>
+            <NavLink to="/profile" className={({ isActive }) => isActive ? "bg-blue-600 flex justify-start gap-2 text-white px-3 py-2 rounded" : "text-gray-600 flex justify-start gap-2 hover:bg-gray-200 px-3 py-2 rounded"}>
                 {({ isActive }) =>
                 <>
                 <img className={`w-5 h-5 transition ${ isActive ? "invert brightness-0" : "" }`} src="/icons/2 User.svg" alt="profile" /><span>Profile</span>
