9ecf4e55d6e3553924c63a26c8a46b03a2b7d316
ilhammursidi <imursidi37@gmail.com>
2026-06-01 03:56:20 +0700

    feat: add async thunk fetch wallet info to dashboard

---
+++ src/Redux/thunks/balance.js
@@ -0,0 +1,27 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const getBalance = createAsyncThunk("/users/dashboard", 
+    async (_, thunkAPI) => {
+        try {
+            const token = localStorage.getItem("user_token")
+            const response = await fetch(`${API_URL}/users/dashboard`, {
+                method: "GET",
+                headers : {
+                    "Content-Type":"application/json",
+                    "X-koda-X":"true",
+                    "Authorization":`Bearer ${token}`
+                },
+            })
+            const result = await response.json();
+
+            if(!response.ok) {
+                return thunkAPI.rejectWithValue(result.error || "Failed get Cashflow info");
+            }
+            return result;
+            
+            } catch (error) {
+            return thunkAPI.rejectWithValue(error.message || "Connection error");
+        }
+    })
