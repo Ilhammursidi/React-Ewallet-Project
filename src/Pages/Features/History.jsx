b97694d926fd7ed91c2abb70f03fd1363d69eb0e
ilhammursidi <imursidi37@gmail.com>
2026-06-22 21:27:03 +0700

    refactor: make a display condition in history table

--- src/Pages/Features/History.jsx
+++ src/Pages/Features/History.jsx
@@ -12,28 +12,87 @@ export const History = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [selectData, setSelectData] = useState(null);
     const dispatch = useDispatch();
+    const BACKEND_URL = import.meta.env.VITE_API_URL
+    const { history, totalPages = 1, data: currentUser, isLoading, error: isError } = useSelector((state) => state.users);
 
-    const { history, totalPages = 1, currentUser, isLoading, isError } = useSelector((state) => state.users);
-    
     const query = searchParams.get("search") || "";
     const currentPage = parseInt(searchParams.get("page") || "1", 10);
+    const currentItems = history || [];
 
     useEffect(() => {
         dispatch(getTransactionHistory({ page: currentPage, search: query }));
     }, [dispatch, currentPage, query]);
 
-    const currentItems = history || [];
+    const [searchInput, setSearchInput] = useState(query);
+
+useEffect(() => {
+    const timer = setTimeout(() => {
+        const trimmed = searchInput.trim();
+        if (trimmed !== query) {
+            setSearchParams({ search: trimmed, page: "1" });
+        }
+    }, 500);
+    return () => clearTimeout(timer);
+}, [searchInput]);
+
+useEffect(() => {
+    setSearchInput(query);
+}, [query]);
+
+const triggerSearchNow = () => {
+    const trimmed = searchInput.trim();
+    setSearchParams({ search: trimmed, page: "1" });
+};
+
 
     const paginate = (pageNumber) => {
         setSearchParams({ search: query, page: pageNumber.toString() });
     };
-    
+
     const handleRowClick = (row) => {
         setSelectData(row);
         setIsOpen(true);
     };
 
-    const isModalTypeTopUp = selectData?.type?.toUpperCase().replace(/\s+/g, '') === "TOPUP";
+    const isModalTopUp = selectData?.type?.toUpperCase().replace(/\s+/g, '') === "TOPUP";
+
+    const isModalTransferIn = selectData?.type?.toUpperCase().replace(/\s+/g, '') === "TRANSFER" && String(currentUser?.id) === String(selectData?.receiver_id);
+
+    const isModalTransferOut = selectData?.type?.toUpperCase().replace(/\s+/g, '') === "TRANSFER" && String(currentUser?.id) === String(selectData?.sender_id);
+
+    const userPhoto = currentUser?.photo?.length > 0 ? currentUser.photo : "img/profiles/user_1781943518142517600.svg";
+
+    let otherPhoto = "-"
+    if (isModalTransferIn) {
+        otherPhoto = selectData?.sender_photo || "img/profiles/user_1781943518142517600.svg";
+    } else if (isModalTransferOut) {
+        otherPhoto = selectData?.receiver_photo || "img/profiles/user_1781943518142517600.svg";
+    }
+    
+    let displayName = "-"
+    if (isModalTopUp) {
+        displayName = "Top Up";
+    } else if (isModalTransferIn) {
+        displayName = selectData?.sender_name;
+    } else if (isModalTransferOut) {
+        displayName = selectData?.receiver_name;
+    }
+
+    let status = "-"
+    if (isModalTopUp) {
+        status = "Top Up Success";
+    } else if (isModalTransferIn) {
+        status = "Receive Success"
+    } else if (isModalTransferOut) {
+        status = "Transfer Success"
+    }
+
+    let phoneNumber = "-"
+    if (isModalTransferIn) {
+        phoneNumber = selectData?.sender_number;
+    } else if (isModalTransferOut) {
+        phoneNumber = selectData?.phone_number;
+    }
 
     return (
         <section>
@@ -42,29 +101,29 @@ export const History = () => {
             {/* MODAL DETAIL */}
             <Modal isOpen={isOpen} value="Back" onClose={() => setIsOpen(false)}>
                 {selectData && (
-                    <section className="flex bg-white flex-col w-80 p-2 md:h-115 rounded-md h-fit">
+                            <section className="flex bg-white flex-col w-80 p-2 md:h-115 rounded-md h-fit">
                         <p className="text-sm font-semibold mb-2">DETAIL TRANSACTION</p>
                         <hr className="border border-gray-200" />
-                        <img 
-                            className="w-20 h-20 rounded-full mx-auto my-3 object-cover" 
-                            src={isModalTypeTopUp ? selectData.receiver_photo : (selectData.sender_photo || "/icons/userone.svg")} 
-                            alt={selectData.name || "Transaction"} 
-                        />
+                        <img
+                            className="w-20 h-20 rounded-full mx-auto my-3 object-cover"
+                            src={isModalTopUp ? `${BACKEND_URL}/${userPhoto}` : `${BACKEND_URL}/${otherPhoto}`}
+                            alt={selectData.name || "Transaction"}
+                            />
 
                         <p className="font-semibold text-xs text-gray-500 mt-1">Name :</p>
-                        <p className="text-sm font-medium">{selectData.receiver_name || "Unknown"}</p>
+                        <p className="text-sm font-medium">{displayName}</p>
 
                         <p className="font-semibold text-xs text-gray-500 mt-1">Phone :</p>
-                        <p className="text-sm font-medium">{selectData.phone || "-"}</p>
+                        <p className="text-sm font-medium">{phoneNumber}</p>
 
                         <p className="font-semibold text-xs text-gray-500 mt-1">Status :</p>
-                        <p className={isModalTypeTopUp ? "text-green-500 text-sm font-medium" : "text-red-500 text-sm font-medium"}>
-                            {isModalTypeTopUp ? "Top Up Success" : "Transfer Success"}
+                        <p className={isModalTopUp || isModalTransferIn ? "text-green-500 text-sm font-medium" : "text-red-500 text-sm font-medium"}>
+                            {status}
                         </p>
 
                         <p className="font-semibold text-xs text-gray-500 mt-1">Amount :</p>
-                        <p className={isModalTypeTopUp ? "text-green-500 font-semibold" : "text-red-500 font-semibold"}>
-                            {isModalTypeTopUp ? `+ ${selectData.amount}` : `- ${selectData.amount}`}
+                        <p className={isModalTopUp || isModalTransferIn ? "text-green-500 font-semibold" : "text-red-500 font-semibold"}>
+                            {isModalTopUp || isModalTransferIn ? `+ Rp ${selectData.amount.toLocaleString("id-ID")}` : `- Rp ${selectData.amount.toLocaleString("id-ID")}`}
                         </p>
 
                         <Button className="flex flex-row items-center justify-center w-full border p-2 mt-4 border-red-600 rounded">
@@ -92,12 +151,15 @@ export const History = () => {
                             <section className="flex items-center justify-between p-2 border border-gray-500 rounded mt-1 focus-within:border-blue-600">
                                 <input
                                     type="text"
-                                    value={query}
-                                    onChange={(e) => setSearchParams({ search: e.target.value, page: "1" })}
+                                    value={searchInput}
+                                    onChange={(e)=> setSearchInput(e.target.value)}
+                                    onKeyDown={(e)=>{
+                                        if(e.key === "Enter") triggerSearchNow();
+                                    }}
                                     placeholder="Enter Number Or Fullname"
                                     className="focus:outline-none bg-transparent"
                                 />
-                                <img src="/icons/Search.svg" alt="search" />
+                                <img src="/icons/Search.svg" onClick={triggerSearchNow} alt="search" />
                             </section>
                         </section>
 
@@ -116,23 +178,58 @@ export const History = () => {
                                         ) : (
                                             currentItems.map((row) => {
                                                 const isRowTopUp = row.type?.toUpperCase().replace(/\s+/g, '') === "TOPUP";
+
+                                                const isRowTransferIn = row.type?.toUpperCase().replace(/\s+/g, '') === "TRANSFER" && String(currentUser?.id) === String(row.receiver_id);
+
+                                                const isRowTransferOut = row.type?.toUpperCase().replace(/\s+/g, '') === "TRANSFER" && String(currentUser?.id) === String(row.sender_id);
+
+                                                const userPhoto = currentUser?.photo?.length > 0 ? currentUser.photo : "img/profiles/user_1781943518142517600.svg";
+
+                                                let displayPhoto = "-";
+                                                if (isRowTopUp) {
+                                                    displayPhoto = userPhoto;
+                                                } else if (isRowTransferIn) {
+                                                    displayPhoto = row.sender_photo;
+                                                } else if (isRowTransferOut) {
+                                                    displayPhoto = row.receiver_photo;
+                                                }
+
+                                                let displayPhone = "-";
+                                                if (isRowTransferIn) {
+                                                    displayPhone = row.sender_number;
+                                                } else if (isRowTransferOut) {
+                                                    displayPhone = row.phone_number;
+                                                }
+
+                                                let displayName = "-"
+                                                if (isRowTopUp) {
+                                                    displayName = "Top Up"
+                                                } else if (isRowTransferIn) {
+                                                    displayName = row.sender_name;
+                                                } else if (isRowTransferOut) {
+                                                    displayName = row.receiver_name;
+                                                }
+
                                                 return (
                                                     <tr key={row.transaction_id} className="table-layout w-full text-xs cursor-pointer hover:bg-gray-100 transition-colors">
                                                         <td className="p-2" onClick={() => handleRowClick(row)}>
                                                             <img
                                                                 className="w-10 h-10 rounded-full object-cover m-auto"
-                                                                src={isRowTopUp ? (currentUser?.photoProfile || "/icons/userone.svg") : (row.img || "/icons/userone.svg")}
+                                                                src={`${BACKEND_URL}/${displayPhoto}`}
                                                                 alt={row.name}
                                                             />
                                                         </td>
                                                         <td className="p-2" onClick={() => handleRowClick(row)}>
-                                                            {isRowTopUp ? (currentUser?.fullName || currentUser?.email?.split("@")[0] || "Me") : row.name}
+                                                            {displayName}
                                                         </td>
-                                                        <td className="p-2" onClick={() => handleRowClick(row)}>
-                                                            {isRowTopUp ? (currentUser?.phone || "-") : row.phone}
+                                                        <td className="p-2 text-center" onClick={() => handleRowClick(row)}>
+                                                            {isRowTopUp ? row.payment_method_name : "E-Wallet" }
                                                         </td>
-                                                        <td className={`p-2 font-medium ${isRowTopUp ? "text-green-500" : "text-red-500"}`} onClick={() => handleRowClick(row)}>
-                                                            {isRowTopUp ? `+ ${row.amount.toLocaleString("id-ID")}` : `- ${row.amount.toLocaleString("id-ID")}`}
+                                                        <td className="p-2 text-center" onClick={() => handleRowClick(row)}>
+                                                            {displayPhone}
+                                                        </td>
+                                                        <td className={`p-2 font-medium text-center ${isRowTopUp || isRowTransferIn ? "text-green-500" : "text-red-500"}`} onClick={() => handleRowClick(row)}>
+                                                            {isRowTopUp || isRowTransferIn ? `+ Rp ${row.amount.toLocaleString("id-ID")}` : `- Rp ${row.amount.toLocaleString("id-ID")}`}
                                                         </td>
                                                         <td className="p-2 text-center">
                                                             <button className="hover:opacity-60 transition-opacity">
@@ -141,8 +238,9 @@ export const History = () => {
                                                         </td>
                                                     </tr>
                                                 );
-                                            })
+                                              })
                                         )}
+                            
                                     </tbody>
                                 </table>
                             )}
